07e2716664e599f7e2d1967dec61029b40dd70f4
Marcin Sojka <marcin_sojka@yahoo.com>
2020-08-23 14:45:00 +0200

    IIS server doesn't work

---
+++ Areas/Admin/Controllers/GenreController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Books.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Books.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GenreController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public GenreController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        // GET Index Action
+        // --------------------------
+        public  IActionResult Index()
+        {
+            // Return list of genres
+            // --------------------------
+            //return View(await _db.Genre.ToListAsync());
+            return View();
+        }
+    }
+}
