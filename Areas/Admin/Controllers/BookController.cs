0b54bddc08a25130ecb2dbcbb4d6bcc5093c9ec6
Marcin Sojka <marcin_sojka@yahoo.com>
2020-09-26 01:02:19 +0200

    - Book Index controller action and view updated - Book Create get action updated - BookViewModel created

--- Areas/Admin/Controllers/BookController.cs
+++ Areas/Admin/Controllers/BookController.cs
@@ -7,35 +7,101 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Books.Data;
 using Books.Models;
+using Microsoft.AspNetCore.Hosting;
+using Books.Models.ViewModels;
 
 namespace Books.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class BookController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webhostEnvironment;
 
-        public BookController(ApplicationDbContext context)
+        [BindProperty]
+        public BookViewModel BookVM { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public BookController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
-            _context = context;
+            _db = db;
+            _webhostEnvironment = webHostEnvironment;
         }
 
         // GET: Admin/Book
+        // =================
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Book.Include(b => b.Author).Include(b => b.Cover).Include(b => b.Genre).Include(b => b.Language).Include(b => b.Publisher);
-            return View(await applicationDbContext.ToListAsync());
+            // Get all books from database
+            // ----------------------
+            var books = _db.Book.Include(b => b.Author).Include(b => b.Cover)
+                                               .Include(b => b.Genre).Include(b => b.Language)
+                                               .Include(b => b.Publisher)
+                                               .OrderBy(b => b.Author.Alias).ThenBy(b => b.Name)
+                                               .ToListAsync();
+
+            // Return books to View
+            // ----------------------
+
+            return View(await books);
+
         }
 
-        // GET: Admin/Book/Details/5
+
+        // GET: Admin/Book/Create
+        // =================
+        public async Task<IActionResult> Create()
+        {
+            BookVM = new BookViewModel
+            {
+
+                Authors = await _db.Author.ToListAsync(),
+                Covers = await _db.Cover.ToListAsync(),
+                Genres = await _db.Genre.Include(a => a.Category).ToListAsync(),
+                Publishers = await _db.Publisher.ToListAsync(),
+                Languages = await _db.Language.ToListAsync()
+
+            };
+            
+
+            return View(BookVM);
+        }
+
+        // POST: Admin/Book/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create()
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _db.Add(book);
+        //        await _db.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["AuthorId"] = new SelectList(_db.Author, "Id", "Name", book.AuthorId);
+        //    ViewData["CoverId"] = new SelectList(_db.Cover, "Id", "Name", book.CoverId);
+        //    ViewData["GenreId"] = new SelectList(_db.Genre, "Id", "Name", book.GenreId);
+        //    ViewData["LanguageId"] = new SelectList(_db.Language, "Id", "Name", book.LanguageId);
+        //    ViewData["PublisherId"] = new SelectList(_db.Publisher, "Id", "Name", book.PublisherId);
+        //    return View(book);
+        //}
+
+
+        // GET: Admin/Book/Details/id
+        // =================
         public async Task<IActionResult> Details(long? id)
         {
+
+            // Verify if Book with id exists
+            // ---------------------- 
             if (id == null)
             {
                 return NotFound();
             }
 
-            var book = await _context.Book
+            var book = await _db.Book
                 .Include(b => b.Author)
                 .Include(b => b.Cover)
                 .Include(b => b.Genre)
@@ -50,37 +116,6 @@ namespace Books.Areas.Admin.Controllers
             return View(book);
         }
 
-        // GET: Admin/Book/Create
-        public IActionResult Create()
-        {
-            ViewData["AuthorId"] = new SelectList(_context.Author, "Id", "Name");
-            ViewData["CoverId"] = new SelectList(_context.Cover, "Id", "Name");
-            ViewData["GenreId"] = new SelectList(_context.Genre, "Id", "Name");
-            ViewData["LanguageId"] = new SelectList(_context.Language, "Id", "Name");
-            ViewData["PublisherId"] = new SelectList(_context.Publisher, "Id", "Name");
-            return View();
-        }
-
-        // POST: Admin/Book/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,AuthorId,ReleaseDate,CoverId,GenreId,PublisherId,LanguageId,Image,Description,OnStock,Type,Price")] Book book)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(book);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["AuthorId"] = new SelectList(_context.Author, "Id", "Name", book.AuthorId);
-            ViewData["CoverId"] = new SelectList(_context.Cover, "Id", "Name", book.CoverId);
-            ViewData["GenreId"] = new SelectList(_context.Genre, "Id", "Name", book.GenreId);
-            ViewData["LanguageId"] = new SelectList(_context.Language, "Id", "Name", book.LanguageId);
-            ViewData["PublisherId"] = new SelectList(_context.Publisher, "Id", "Name", book.PublisherId);
-            return View(book);
-        }
 
         // GET: Admin/Book/Edit/5
         public async Task<IActionResult> Edit(long? id)
@@ -90,16 +125,16 @@ namespace Books.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var book = await _context.Book.FindAsync(id);
+            var book = await _db.Book.FindAsync(id);
             if (book == null)
             {
                 return NotFound();
             }
-            ViewData["AuthorId"] = new SelectList(_context.Author, "Id", "Name", book.AuthorId);
-            ViewData["CoverId"] = new SelectList(_context.Cover, "Id", "Name", book.CoverId);
-            ViewData["GenreId"] = new SelectList(_context.Genre, "Id", "Name", book.GenreId);
-            ViewData["LanguageId"] = new SelectList(_context.Language, "Id", "Name", book.LanguageId);
-            ViewData["PublisherId"] = new SelectList(_context.Publisher, "Id", "Name", book.PublisherId);
+            ViewData["AuthorId"] = new SelectList(_db.Author, "Id", "Name", book.AuthorId);
+            ViewData["CoverId"] = new SelectList(_db.Cover, "Id", "Name", book.CoverId);
+            ViewData["GenreId"] = new SelectList(_db.Genre, "Id", "Name", book.GenreId);
+            ViewData["LanguageId"] = new SelectList(_db.Language, "Id", "Name", book.LanguageId);
+            ViewData["PublisherId"] = new SelectList(_db.Publisher, "Id", "Name", book.PublisherId);
             return View(book);
         }
 
@@ -119,8 +154,8 @@ namespace Books.Areas.Admin.Controllers
             {
                 try
                 {
-                    _context.Update(book);
-                    await _context.SaveChangesAsync();
+                    _db.Update(book);
+                    await _db.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -135,11 +170,11 @@ namespace Books.Areas.Admin.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AuthorId"] = new SelectList(_context.Author, "Id", "Name", book.AuthorId);
-            ViewData["CoverId"] = new SelectList(_context.Cover, "Id", "Name", book.CoverId);
-            ViewData["GenreId"] = new SelectList(_context.Genre, "Id", "Name", book.GenreId);
-            ViewData["LanguageId"] = new SelectList(_context.Language, "Id", "Name", book.LanguageId);
-            ViewData["PublisherId"] = new SelectList(_context.Publisher, "Id", "Name", book.PublisherId);
+            ViewData["AuthorId"] = new SelectList(_db.Author, "Id", "Name", book.AuthorId);
+            ViewData["CoverId"] = new SelectList(_db.Cover, "Id", "Name", book.CoverId);
+            ViewData["GenreId"] = new SelectList(_db.Genre, "Id", "Name", book.GenreId);
+            ViewData["LanguageId"] = new SelectList(_db.Language, "Id", "Name", book.LanguageId);
+            ViewData["PublisherId"] = new SelectList(_db.Publisher, "Id", "Name", book.PublisherId);
             return View(book);
         }
 
@@ -151,7 +186,7 @@ namespace Books.Areas.Admin.Controllers
                 return NotFound();
             }
 
-            var book = await _context.Book
+            var book = await _db.Book
                 .Include(b => b.Author)
                 .Include(b => b.Cover)
                 .Include(b => b.Genre)
@@ -171,15 +206,15 @@ namespace Books.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(long id)
         {
-            var book = await _context.Book.FindAsync(id);
-            _context.Book.Remove(book);
-            await _context.SaveChangesAsync();
+            var book = await _db.Book.FindAsync(id);
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool BookExists(long id)
         {
-            return _context.Book.Any(e => e.Id == id);
+            return _db.Book.Any(e => e.Id == id);
         }
     }
 }
---
+++ Models/ViewModels/BookViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Models.ViewModels
+{
+    public class BookViewModel
+    {
+        public Book Book { get; set; }
+        public Author Author { get; set; }
+        public Language Language { get; set; }
+        public Cover Cover { get; set; }
+        public Genre Genre { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Author> Authors { get; set; }
+        public IEnumerable<Language> Languages { get; set; }
+        public IEnumerable<Cover> Covers { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public string StatusMessage { get; set; }
+
+    }
+}
